1cefef5fcceac92deb41968bcac362fae6347e70
Matthew Arenas <140763896+marenas1@users.noreply.github.com>
2024-12-02 22:02:28 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/marenas1.github.io/',
+  base: '/marenas1.github.io/marenas1.github.io',
   plugins: [react()],
 })
